0c819ace5dee9b0e0372cf99c9c28f345e1f86f1
eelissal <elissalde.elsa@gmail.com>
2025-06-16 18:26:59 +0200

    add: builtin cd

--- src/exec/builtin/cd.c
+++ src/exec/builtin/cd.c
@@ -6,49 +6,131 @@
 /*   By: eelissal <eelissal@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 16:43:34 by eelissal          #+#    #+#             */
-/*   Updated: 2025/06/14 14:33:34 by eelissal         ###   ########lyon.fr   */
+/*   Updated: 2025/06/16 16:52:36 by eelissal         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+/*LOGIQUE
 
-int exec_cd()
+access:
+	checks file permissions or existence. Used in cd to verify directory existence
+getcwd:
+	gets the current working directory. Core for pwd and for updating environment variables on cd
+chdir:
+	changes the current working directory. Essential for cd.
+stat / lstat / fstat:
+	retrieves file or directory status. Used for checking file types, permissions, or existence, especially in cd.
+*/
+
+char	*ft_strcut(const char *s, size_t start)
 {
-	
+	size_t	len;
+	char	*new;
+
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	if (start > len)
+		start = len;
+	new = malloc(sizeof(char) * (start + 1));
+	if (!new)
+		return (NULL);
+	ft_memcpy(new, s, start);
+	new[start] = '\0';
+	return (new);
 }
 
-int	cd_get_path(t_exec *exec)
+char	*ft_strndup(const char *str, size_t n)
 {
-	//cd avec ., se deplacer la ou on est: prendre pwd (ne fait rien sauf rafraîchir certaines variables d'environnement: PWD et OLDPWD)
-	//cd avec .. se deplacer dans le dossier d'au dessus (le dossier parent): prendre pwd et enlever le dernier dossier sauf s'il n'y a plus qu'un / alors ne rien faire et pas d'erreur
-	//cd avec une absolute path, se deplacer dans le dossier correspondant: prendre pwd + rajouter l'absolute path + check si c'est un directory et qu'il existe
-	char oldpwd[PATH_MAX];
+	size_t	i;
+	char	*dup;
+
+	if (!str)
+		return (NULL);
+	dup = malloc(sizeof(char) * (n + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < n && str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+
+/*
+cd avec ., se deplacer la ou on est: prendre pwd (ne fait rien sauf rafraîchir certaines variables d'environnement: PWD et OLDPWD)
+cd avec .. se deplacer dans le dossier d'au dessus (le dossier parent): prendre pwd et enlever le dernier dossier sauf s'il n'y a plus qu'un / alors ne rien faire et pas d'erreur
+/cd avec une absolute path, se deplacer dans le dossier correspondant: prendre pwd + rajouter l'absolute path + check si c'est un directory et qu'il existe
+*/
+int	exec_cd(char *newpwd)
+{
+	struct stat	info;
+	
+	if (access(newpwd, F_OK | X_OK) != 0)
+	{
+		printf("cd: %s: %s\n", newpwd, strerror(errno));
+		return (1);
+	}
+	if (!S_ISDIR(info.st_mode))
+	{
+		printf("cd: %s: %s\n", newpwd, strerror(ENOTDIR));
+		return (1);
+	}
+	if (chdir(newpwd) != 0)
+	{
+		perror("cd: chdir fail");
+		return (1);
+	}
+	return (0);
+}
+
+int	cd_get_path(char *target)
+{
+	char		oldpwd[PATH_MAX];
+	char		*newpwd;
 
 	if (!getcwd(oldpwd, sizeof(oldpwd)))
 	{
 		perror("getcwd error");
 		return (1);
 	}
-	//si . >
-	//si .. >
-	//si absolute
+	if (ft_strcmp(target, ".") == 0)
+		newpwd = ft_strdup(oldpwd);
+	else if (ft_strcmp(target, "..") == 0)
+		newpwd = ft_strndup(oldpwd, ft_strrchr(oldpwd, '/'));
+	else
+		newpwd = ft_strjoin(oldpwd, target);
+	if (!newpwd)
+		return (1);
+	if (exec_cd(newpwd) != 0)
+	{
+		free(newpwd);
+		return (1);
+	}
 	return (0);
 }
 
-int	cd_home(t_exec *exec)
+//"HOME" pour target
+int	cd_home(char *target)
 {
-	char	*target;
+	char		*newpwd;
+	struct stat	info;
 
-	target = getenv("HOME");
-	if (!target)
+	newpwd = getenv(target);
+	if (!newpwd)
 	{
-		ft_putstr_fd("cd: HOME not set\n", STDERR_FILENO);
+		printf("cd: %s not set\n", target);
 		return (1);
 	}
-	if (chdir(target) != 0)
+	if (exec_cd(newpwd) != 0)
 	{
-		perror("cd");
+		free(newpwd);
 		return (1);
 	}
 	return (0);
@@ -56,17 +138,13 @@ int	cd_home(t_exec *exec)
 
 int	builtin_cd(t_exec *exec)
 {
-	if (!exec->current->cmd)
-		return (CMD_NOT_FOUND);
-	else if (!exec->current->cmd->data[0])
-		return (CMD_NOT_FOUND);
-	else if (exec->current->cmd->data[0] && !exec->current->cmd->data[1])
-		return (cd_home(exec));
+	if (exec->current->cmd->data[0] && !exec->current->cmd->data[1])
+		return (cd_home("HOME"));
 	else if (exec->current->cmd->data[1] && !exec->current->cmd->data[2])
-		return (cd_get_path(exec));
+		return (cd_get_path(exec->current->cmd->data[1]));
 	else if (exec->current->cmd->data[2])
 	{
-		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
 		return (1);
 	}
 	return (1);
--- src/exec/builtin/env.c
+++ src/exec/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: eelissal <eelissal@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 14:10:42 by eelissal          #+#    #+#             */
-/*   Updated: 2025/06/14 13:59:21 by eelissal         ###   ########lyon.fr   */
+/*   Updated: 2025/06/14 14:42:58 by eelissal         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	builtin_env(t_exec *exec)
 	i = 0;
 	if (exec->current->cmd->count > 1)
 	{
-		ft_putstr_fd("minishell: env function cannot take any options or arguments\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: env: too many arguments\n", STDERR_FILENO);
 		return (EXIT_FAILURE);
 	}
 	while (i < exec->shell->env->count)
