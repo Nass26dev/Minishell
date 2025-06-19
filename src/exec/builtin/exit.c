d62e0e01f524ac165d89f15b12b68282cda74d02
eelissal <elissalde.elsa@gmail.com>
2025-06-19 16:01:43 +0200

    fix: builtin: exit

--- src/exec/builtin/exit.c
+++ src/exec/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: eelissal <eelissal@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 11:59:00 by eelissal          #+#    #+#             */
-/*   Updated: 2025/06/19 13:25:07 by eelissal         ###   ########lyon.fr   */
+/*   Updated: 2025/06/19 15:52:29 by eelissal         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,19 @@ int	arg_is_digit(t_exec *exec)
 	char	*data;
 	int		j;
 
-	data = exec->current->cmd->data[1];
+	data = exec->current->command[1];
 	j = 0;
 	if (data[0] == '+' || data[0] == '-')
 		j++;
 	while (data[j])
 	{
-		if (ft_isdigit(data[j]) != 0)
+		if (ft_isdigit(data[j]) == 0)
 		{
 			printf("exit\n");
 			printf("minishell: exit: %s: numeric argument required\n", data);
 			free_shell(exec->shell);
 			exec->shell->status = 2;
-			return (exec->shell->status);
+			return (1);
 		}
 		j++;
 	}
@@ -53,7 +53,7 @@ int	builtin_exit(t_exec *exec)
 	int	result;
 
 	i = 0;
-	while (exec->current->cmd->data[i])
+	while (exec->current->command[i])
 		i++;
 	if (i == 1)
 	{
@@ -64,7 +64,7 @@ int	builtin_exit(t_exec *exec)
 	}
 	if (i > 2)
 	{
-		if (!arg_is_digit(exec))
+		if (arg_is_digit(exec) != 0)
 			exit(exec->shell->status);
 		else
 		{
@@ -75,10 +75,12 @@ int	builtin_exit(t_exec *exec)
 	}
 	if (i == 2)
 	{
-		if (!arg_is_digit(exec))
+		if (arg_is_digit(exec) != 0)
 			exit(exec->shell->status);
-		result = ft_atoi(exec->current->cmd->data[1]);
+		result = ft_atoi(exec->current->command[1]);
 		exec->shell->status = check_overflow(result);
+		free_shell(exec->shell);
+		rl_clear_history();
 		exit (exec->shell->status);
 	}
 	return (1);
