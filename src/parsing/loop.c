afe41a39e68d5b571a71d583b298eacd72d2f60a
Nassim Yousfi <nyousfi@z4r10p6.42lyon.fr>
2025-06-30 19:14:51 +0200

    wip

--- src/parsing/loop.c
+++ src/parsing/loop.c
@@ -6,7 +6,7 @@
 /*   By: nyousfi <nyousfi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 13:33:45 by nyousfi           #+#    #+#             */
-/*   Updated: 2025/06/30 18:26:51 by nyousfi          ###   ########.fr       */
+/*   Updated: 2025/06/30 19:06:08 by nyousfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,48 +55,89 @@ bool	else_case(char *input, int *index)
 	return (false);
 }
 
-bool	check_redir_error(char *input, t_data *data)
-{
-	int	i;
+// bool	check_redir_error(char *input, t_data *data)
+// {
+// 	int	i;
+// 	int count;
 
-	i = 0;
-	while (input[i])
-	{
-		if (input[i] == '<' && input[i + 1] == '<')
-		{
-			print_correct_error(HEREDOC);
-			data->shell->status = 2;
-			return (true);
-		}
-		if (char_is_redir(input[i]) && char_is_redir(input[i + 1]))
-			i++;
-		if (char_is_redir(input[i]) && input[i + 1] && input[i + 1] == ' '
-			&& input[i + 2] && is_operator(input[i + 2]))
-		{
-			print_correct_error(find_correct_tag(input[i]));
-			data->shell->status = 2;
-			return (true);
-		}
-		if (input[i] == '"')
-		{
-			i++;
-			while (input[i] != '"' && input[i])
-				i++;
-		}
-		if (else_case(input, &i))
-		{
-			data->shell->status = 2;
-			return (true);
-		}
-		i++;
-	}
-	return (false);
-}
+// 	count = 0;
+// 	i = 0;
+// 	while (input[i])
+// 	{
+// 		count = 0;
+// 		if (input[i] == '<')
+// 		{
+// 			while (input[i] == '<')
+// 			{
+// 				count++;
+// 				i++;
+// 			}
+// 			if (count > 2)
+// 			{
+// 				print_correct_error(HEREDOC);
+// 				data->shell->status = 2;
+// 				return (true);
+// 			}
+// 		}
+// 		else if (input[i] == '>')
+// 		{
+// 			while (input[i] == '>')
+// 			{
+// 				count++;
+// 				i++;
+// 			}
+// 			if (count > 2)
+// 			{
+// 				print_correct_error(APPEND);
+// 				data->shell->status = 2;
+// 				return (true);
+// 			}
+// 		}
+// 		else
+// 			i++;
+// 	}
+// 	i = 0;
+// 	while (input[i])
+// 	{
+// 		// if (input[i] == '<' && input[i + 1] == '<' && input[i + 2] && !input[i + 2])
+// 		// {
+// 		// 	print_correct_error(HEREDOC);
+// 		// 	data->shell->status = 2;
+// 		// 	return (true);
+// 		// }
+// 		// {
+// 		// 	print_correct_error(HEREDOC);
+// 		// 	data->shell->status = 2;
+// 		// 	return (true);
+// 		// }
+// 		if (char_is_redir(input[i]) && char_is_redir(input[i + 1]))
+// 			i++;
+// 		if (char_is_redir(input[i]) && input[i + 1] && input[i + 1] == ' '
+// 			&& input[i + 2] && is_operator(input[i + 2]))
+// 		{
+// 			print_correct_error(find_correct_tag(input[i]));
+// 			data->shell->status = 2;
+// 			return (true);
+// 		}
+// 		if (input[i] == '"')
+// 		{
+// 			i++;
+// 			while (input[i] != '"' && input[i])
+// 				i++;
+// 		}
+// 		if (else_case(input, &i))
+// 		{
+// 			data->shell->status = 2;
+// 			return (true);
+// 		}
+// 		i++;
+// 	}
+// 	return (false);
+// }
 
 bool	mltpl_check(t_data *data, char *input)
 {
-	if (!input[0] || is_only_spaces(input) || is_only_quotes(input)
-		|| check_redir_error(input, data))
+	if (!input[0] || is_only_spaces(input) || is_only_quotes(input))
 	{	
 		free(input);
 		return (true);
