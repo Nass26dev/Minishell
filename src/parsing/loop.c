38c34da4f50bd09a72dab022800ad76a5e9882b7
Nassim Yousfi <nyousfi@z3r4p3.42lyon.fr>
2025-07-16 08:46:44 +0200

    remove free ast

--- src/parsing/loop.c
+++ src/parsing/loop.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   loop.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nass <nass@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nyousfi <nyousfi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 13:33:45 by nyousfi           #+#    #+#             */
-/*   Updated: 2025/07/15 21:44:20 by nass             ###   ########.fr       */
+/*   Updated: 2025/07/16 08:46:30 by nyousfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,7 +124,6 @@ int	minishell_loop(t_shell *shell)
 		return (data.shell->status);
 	}
 	data.ast = parser(&data, data.tokens, find_last_node(data.tokens));
-	print_ast(data.ast, 0);
 	free_tokens(&data.tokens);
 	if (data.ast)
 		data.shell->status = execute(data.ast, data.shell, data.heredoc);
